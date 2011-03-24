622f3a5dc5fac858e3bba904580319f018a99376
Gregg Lind <gregg.lind@gmail.com>
2011-03-24 15:39:10 -0500

    added __unicode__ methods

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,14 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
 
+    def __unicode__(self):
+        return self.question
+
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField()
+
+    def __unicode__(self):
+        return self.choice
+
