1f4413259cf367c2d4c7fcb621c34c6c6ca49f87
Gregg Lind <gregg.lind@gmail.com>
2011-03-24 21:23:20 -0500

    'last 5' index view.  Causes 'TemplateDoesNotExist' error.

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,19 @@
 # Create your views here.
 
-from polls.models import Poll
+
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+
+from polls.models import Poll
 
 def redirect_to_polls(request):
     return HttpResponseRedirect('/polls/')
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    context = {'latest_poll_list': latest_poll_list}
+    return render_to_response('polls/index.html', context)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % (poll_id,))
