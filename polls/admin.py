d3e4c51ecb0f787696458fd72f41c44c4f5cd3b3
Gregg Lind <gregg.lind@gmail.com>
2011-03-24 23:13:40 -0500

    stacked choices for polls in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,15 @@
-from polls.models import Poll
+from polls.models import Poll,Choice
 from django.contrib import admin
 
-admin.site.register(Poll)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
